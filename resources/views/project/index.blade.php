2453048b7e919344e3ce04ad4fee5163b53abce7
Skysoul <rafael@skysoul.com.au>
2015-10-10 18:11:03 +0800

    fix

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -5,42 +5,35 @@
     <div class="column">
 
 
-            @foreach($projects as $project)
+        @foreach($projects as $project)
 
-                    <div class="ui fluid card">
-                        <div class="content">
-                            <i class="right floated like icon"></i>
-                            <i class="right floated star icon"></i>
+            {{--<div class="ui fluid card">--}}
+                {{--<div class="content">--}}
+                    {{--<i class="right floated like icon"></i>--}}
+                    {{--<i class="right floated star icon"></i>--}}
 
-                            <div class="header">Cute Dog</div>
-                            <div class="description">
-                                <p></p>
-                            </div>
-                        </div>
-                        <div class="extra content">
-    <span class="left floated like">
-      <i class="like icon"></i>
-      Like
-    </span>
-    <span class="right floated star">
-      <i class="star icon"></i>
-      Favorite
-    </span>
-                        </div>
-                    </div>
-                </div>
-
-                {{--<div class="ui segment">--}}
-                    {{--<a href="{{ route('project.show', $project->id) }}" class="title">{{ $project->title }}</a>--}}
-
-                    {{--<div class="ui right floated">--}}
-                        {{--<div class="ui heart rating" data-rating="1" data-max-rating="3"></div>--}}
+                    {{--<div class="header">Cute Dog</div>--}}
+                    {{--<div class="description">--}}
+                        {{--<p></p>--}}
                     {{--</div>--}}
-                    {{--<div class="content">{{ $project->description }}</div>--}}
                 {{--</div>--}}
-            @endforeach
+                {{--<div class="extra content">--}}
+                    {{--<span class="left floated like"><i class="like icon"></i>Like</span>--}}
+                    {{--<span class="right floated star"><i class="star icon"></i>Favorite</span>--}}
+                {{--</div>--}}
+            {{--</div>--}}
 
-        </div>
+            <div class="ui segment">
+            <a href="{{ route('project.show', $project->id) }}" class="title">{{ $project->title }}</a>
+
+            <div class="ui right floated">
+            <div class="ui heart rating" data-rating="1" data-max-rating="3"></div>
+            </div>
+            <div class="content">{{ $project->description }}</div>
+            </div>
+        @endforeach
+
+    </div>
 
 
 @endsection
