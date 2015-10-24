2ed74e1965d36f28f5ca1379d10507148d91a625
Rafael <rafael.milewski@gmail.com>
2015-10-24 18:35:31 +0800

    Macro in envoy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,9 +1,14 @@
 @servers(['web' => 'skysoul1@skysoul.com.au'])
 
+@macro('reset')
+    remove-clone
+    reset-db
+@endmacro
+
 @task('update')
-cd public_html/dreamsark.dev
-git checkout .
-git pull origin master
+    cd public_html/dreamsark.dev
+    git checkout .
+    git pull origin master
 @endtask
 
 @task('config-git')
@@ -16,21 +21,18 @@ cd public_html/dreamsark.dev
 php56s artisan migrate:refresh --seed
 @endtask
 
-@task('reset')
-
-    mysql -uskysoul1 -pGH4C=3F6
-
-    drop database skysoul1_dreamsark;
-    drop database skysoul1_dreamsark_report;
-    drop database skysoul1_dreamsark_translation;
-
-    create database skysoul1_dreamsark;
-    create database skysoul1_dreamsark_report;
-    create database skysoul1_dreamsark_translation;
-
-    exit;
-
+@task('remove-clone')
     cd public_html
     rm -r -f dreamsark.dev
     git clone https://github.com/SkysoulDesign/Dreamsark.git dreamsark.dev
+@endtask
+
+@task('reset-db')
+    mysql -uskysoul1 -pGH4C=3F6
+    drop database skysoul1_dreamsark;
+    drop database skysoul1_dreamsark_report;
+    drop database skysoul1_dreamsark_translation;
+    create database skysoul1_dreamsark;
+    create database skysoul1_dreamsark_report;
+    create database skysoul1_dreamsark_translation;
 @endtask
