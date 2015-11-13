d9734cf27b7854b87ba8a247317ac87c98461baa
Rafael <rafael@skysoul.com.au>
2015-11-13 10:55:13 +0000

    A lot of Changes

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,20 @@
+<div class="container-fluid header">
+
+    <section id="particles-js" class="particles"></section>
+
+    <div class="row">
+        <section class="medium-8 medium-centered column slider-title">
+            <h1>PROJECT DEMO</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit maecenas elementum, lacus.</p>
+        </section>
+
+        <section class="medium-12 medium-centered column slider">
+            <img src="{{ asset('dreamsark-assets/slider.png') }}" alt="Slider">
+        </section>
+    </div>
+
+    <section class="banner">
+        <h1>See Our Finished Projects List</h1>
+    </section>
+
+</div>
